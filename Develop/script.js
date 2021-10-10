ce45cc3bf79b2122450b17fffc418a517cf4280c
Nathan Nelson <nathan.nelson042@gmail.com>
2021-10-09 23:21:38 -0700

    added a few starting variables and set up the bones to prompt people for the requested criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,46 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var passwordComplexityAttributes ={
+  length: '',
+  upper: '',
+  lower: '',
+  numbers: '',
+  specials: ''
+};
+
+
+//Prompts the user to select desired password parameters
+var promptForAttributes = function() {
+
+  // Ask user if they want lower case letters
+  promptForAttributes.lowercase = window.prompt('Would you like to use lower case letters?');
+
+
+  // Ask if they want upper case letters
+  promptForAttributes.lowercase = window.prompt('Would you like to use upper case letters?');
+
+
+  // Ask desired length of password
+  promptForAttributes.lowercase = window.prompt('How long would you like the password to be?');
+
+
+  // Ask for numbers
+  promptForAttributes.lowercase = window.prompt('Would you like to include numbers?');
+
+
+  // Ask for special characters
+  promptForAttributes.lowercase = window.prompt('Would you like to use special characters?');
+
+}
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
