0b4a3a757b6751bf005acd6c0c228af13efa69bb
Nathan Nelson <nathan.nelson042@gmail.com>
2021-11-19 02:49:00 -0700

    a password will generate if i cancel the loop after one iteration. So it needs to be fixed. currently stuck in a loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,60 @@
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"]
-
-
-
-
-
+var upperList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbolList = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"]
 
 var generateBtn = document.querySelector("#generate");
-
+var finalPassword = "";
 
 
 // prompt for parameters 
 function generatePassword() {
+
   var desiredLength = prompt("How many characters will your password have?");
 
   if (desiredLength <= 8 || desiredLength >= 128) {
     alert("Password length must be 8 or more characters long. MAX character limit is 128!")
-  }
 
-  if (desiredLength >= 8 && desiredLength <= 128) {
-    var upper = confirm("Will your password use uppercase letters?");
-    var lower = confirm("Will your password use lowercase letters?");
-    var number = confirm("Will you password use numbers?");
-    var symbol = confirm("Will you password use special characters?");
+  } else {
+    var upperCon = confirm("Will your password use uppercase letters?");
+    var lowerCon = confirm("Will your password use lowercase letters?");
+    var numberCon = confirm("Will you password use numbers?");
+    var symbolCon = confirm("Will you password use special characters?");
 
+    while (!upperCon && !lowerCon && !numberCon && !symbolCon);
 
+    var options = [];
 
-    // Incase they dont pick at least one option
-    while (!upper && !lower && !number && !symbol) {                              // (thought this was cool. Also for my own future reference) In Javascript, the exclamation mark (“!”) symbol, called a “bang,” is the logical “not” operator. Placed in front of a boolean value it will reverse the value, returning the opposite.
-      alert("Must select AT LEAST one to generate password!");
-      upper = confirm("Will your password use uppercase letters?");
-      lower = confirm("Will your password use lowercase letters?");
-      number = confirm("Will you password use numbers?");
-      symbol = confirm("Will you password use special characters?");
+    if (upperCon) {
+      //options.push(...upperList);
+      options = options.concat(upperList);
+
     }
-  }
-}
 
+    if (lowerCon) {
+      //options.push(...lowerList);
+      options = options.concat(lowerList);
+
+    }
+
+    if (numberCon) {
+      //options.push(...numberList);
+      options = options.concat(numberList);
+
+    }
+
+    if (symbolCon) {
+      //options.push(...symbolList);
+      options = options.concat(symbolList);
+
+    }
+  
+    for (var i = 0; i < desiredLength; i++) {
+      finalPassword = finalPassword + options[Math.floor(Math.random() * options.length)]
+    }
+    return finalPassword;
+  }
+};
 
 
 
@@ -48,8 +64,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+};
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
